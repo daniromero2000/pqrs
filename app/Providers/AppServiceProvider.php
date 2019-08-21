d1ab8df49594571d18dc2cd73e0dc1be567179e9
Standard <desarrolladorcoandes@standard.com.co>
2019-08-21 14:31:36 -0500

    21aout2019

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Cashier\Cashier;
 use Illuminate\Support\Facades\Schema;
 use App\Socomir\Pqrs\Pqr;
 use App\Socomir\Pqrs\Repositories\Interfaces\PqrRepositoryInterface;
