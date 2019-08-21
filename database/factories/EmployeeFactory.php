5760676a05fb97cb310a67293abeede1b980dbff
Standard <desarrolladorcoandes@standard.com.co>
2019-08-21 15:09:40 -0500

    21aout2019

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -6,7 +6,14 @@ use App\Model\Employee;
 use Faker\Generator as Faker;
 
 $factory->define(Employee::class, function (Faker $faker) {
+    static $password;
+
     return [
-        //
+        'name' => $faker->firstName,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+        'status' => 1, 
+        'city' => 'Bogotá'
     ];
 });
