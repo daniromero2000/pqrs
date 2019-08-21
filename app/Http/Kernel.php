59b1ad56786dc0ca24806e3168e045f7e59fee48
Standard <desarrolladorcoandes@standard.com.co>
2019-08-21 14:31:15 -0500

    21aout2019

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'employee' => \App\Http\Middleware\RedirectIfNotEmployee::class,
+        'checkout' => \App\Http\Middleware\RedirectIfNotCustomer::class,
     ];
 
     /**
---
+++ app/Providers/GlobalTemplateServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class GlobalTemplateServiceProvider
+ * @package App\Providers
+ * @codeCoverageIgnore
+ */
+class GlobalTemplateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       
+    }
+     
+}
