e248ed74268e0fc58ee61764dcf3c0deb31f04ce
Dalkyel <j.alejandrocr@gmail.com>
2019-08-21 19:39:17 -0500

    21aout2019

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/**
+ * Admin routes
+ */
+Route::namespace('Admin')->group(function () {
+    Route::get('admin/login', 'LoginController@showLoginForm')->name('admin.login');
+    Route::post('admin/login', 'LoginController@login')->name('admin.login');
+    Route::get('admin/logout', 'LoginController@logout')->name('admin.logout');
+});
+Route::group(['prefix' => 'admin', 'middleware' => ['employee'], 'as' => 'admin.'], function () {
+    Route::namespace('Admin')->group(function () {
+        Route::group(['middleware' => ['role:admin|superadmin|operativo|marketing, guard:employee']], function () {
+            Route::get('/', 'DashboardController@index')->name('dashboard');
+
+            Route::resource('countries', 'Countries\CountryController');
+            Route::resource('countries.provinces', 'Provinces\ProvinceController');
+            Route::resource('countries.provinces.cities', 'Cities\CityController');
+        });
+        Route::group(['middleware' => ['role:admin|superadmin|marketing|operativo, guard:employee']], function () {
+            Route::resource('employees', 'EmployeeController');
+            Route::get('employees/{id}/profile', 'EmployeeController@getProfile')->name('employee.profile');
+            Route::put('employees/{id}/profile', 'EmployeeController@updateProfile')->name('employee.profile.update');
+            Route::resource('roles', 'Roles\RoleController');
+            Route::resource('permissions', 'Permissions\PermissionController');
+        });
+    });
+});
+/**
+ * Frontend routes
+ */
