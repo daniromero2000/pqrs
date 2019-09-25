da6a3dd249609e0acf86336eb6ca099634296c6c
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-09-25 07:52:14 -0500

    25/09/2019, Modulo sucursales automatico

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MyCountryTableSeeder::class);
-        $this->call(MyProvincesTableSeeder::class);
-        $this->call(MyCitiesTableSeeder::class);
+
+
         $this->call(SubsidiariesTableSeeder::class);
         $this->call(EmployeesTableSeeder::class);
-        $this->call(YearsTableSeeder::class);
-        $this->call(PqrStatusTableSeeder::class);
     }
 }
