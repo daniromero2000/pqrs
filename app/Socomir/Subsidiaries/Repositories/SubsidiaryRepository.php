488d5095d5e7d982c8cc59ca2676dd32c69da00a
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-09-26 18:00:25 -0500

    26/09/2019, Modulo sucursales automatico

--- app/Socomir/Subsidiaries/Repositories/SubsidiaryRepository.php
+++ app/Socomir/Subsidiaries/Repositories/SubsidiaryRepository.php
@@ -7,83 +7,44 @@ use App\Socomir\Subsidiaries\Subsidiary;
 use App\Socomir\Subsidiaries\Exceptions\SubsidiaryInvalidArgumentException;
 use App\Socomir\Subsidiaries\Exceptions\SubsidiaryNotFoundException;
 use App\Socomir\Subsidiaries\Repositories\Interfaces\SubsidiaryRepositoryInterface;
-use App\Socomir\Tools\UploadableTrait;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Collection;
 
 class SubsidiaryRepository extends BaseRepository implements SubsidiaryRepositoryInterface
 {
-    use UploadableTrait;
-
-    /**
-     * SubsidiaryRepository constructor.
-     * @param Subsidiary $subsidiary
-     */
     public function __construct(Subsidiary $subsidiary)
     {
         parent::__construct($subsidiary);
         $this->model = $subsidiary;
     }
 
-    /**
-     * List all the subsidiaries
-     *
-     * @param string $order
-     * @param string $sort
-     * @param array $except
-     * @return \Illuminate\Support\Collection
-     */
-    public function listSubsidiaries(string $order = 'id', string $sort = 'desc', $except = []) : Collection
+
+    public function listSubsidiaries(string $order = 'id', string $sort = 'desc', $except = []): Collection
     {
         return $this->model->orderBy($order, $sort)->get()->except($except);
     }
 
-    /**
-     * List all root Subsidiaries
-     * 
-     * @param  string $order 
-     * @param  string $sort  
-     * @param  array  $except
-     * @return \Illuminate\Support\Collection  
-     */
-    public function rootSubsidiaries(string $order = 'id', string $sort = 'desc', $except = []) : Collection
+
+    public function rootSubsidiaries(string $order = 'id', string $sort = 'desc', $except = []): Collection
     {
         return $this->model->whereIsRoot()
-                        ->orderBy($order, $sort)
-                        ->get()
-                        ->except($except);
+            ->orderBy($order, $sort)
+            ->get()
+            ->except($except);
     }
 
-    /**
-     * Create the subsidiary
-     *
-     * @param array $params
-     *
-     * @return Subsidiary
-     * @throws SubsidiaryInvalidArgumentException
-     * @throws SubsidiaryNotFoundException
-     */
-    public function createSubsidiary(array $params) : Subsidiary
+    public function createSubsidiary(array $params): Subsidiary
     {
         try {
             return $this->create($params);
-        }
-        catch (QueryException $e) {
+        } catch (QueryException $e) {
             throw new SubsidiaryInvalidArgumentException($e->getMessage());
         }
     }
 
-    /**
-     * Update the subsidiary
-     *
-     * @param array $params
-     *
-     * @return boolean
-     * @throws SubsidiaryNotFoundException
-     */
-    public function updateSubsidiary(array $params) : bool
+
+    public function updateSubsidiary(array $params): bool
     {
         try {
             return $this->model->update($params);
@@ -92,12 +53,8 @@ class SubsidiaryRepository extends BaseRepository implements SubsidiaryRepositor
         }
     }
 
-    /**
-     * @param int $id
-     * @return Subsidiary
-     * @throws SubsidiaryNotFoundException
-     */
-    public function findSubsidiaryById(int $id) : Subsidiary
+
+    public function findSubsidiaryById(int $id): Subsidiary
     {
         try {
             return $this->findOneOrFail($id);
@@ -106,37 +63,20 @@ class SubsidiaryRepository extends BaseRepository implements SubsidiaryRepositor
         }
     }
 
-    /**
-     * Delete a subsidiary
-     *
-     * @return bool
-     * @throws \Exception
-     */
-    public function deleteSubsidiary() : bool
+
+    public function deleteSubsidiary(): bool
     {
         return $this->model->delete();
     }
 
-    
-       /**
-     * @param $file
-     * @param null $disk
-     * @return bool
-     */
-    public function deleteFile(array $file, $disk = null) : bool
+
+    public function deleteFile(array $file, $disk = null): bool
     {
         return $this->update(['cover' => null], $file['subsidiary']);
     }
 
-    /**
-     * Return the subsidiary by using the slug as the parameter
-     *
-     * @param array $slug
-     *
-     * @return Subsidiary
-     * @throws SubsidiaryNotFoundException
-     */
-    public function findSubsidiaryBySlug(array $slug) : Subsidiary
+
+    public function findSubsidiaryBySlug(array $slug): Subsidiary
     {
         try {
             return $this->findOneByOrFail($slug);
@@ -145,17 +85,13 @@ class SubsidiaryRepository extends BaseRepository implements SubsidiaryRepositor
         }
     }
 
-    /**
-     * @return mixed
-     */
+
     public function findParenSubsidiary()
     {
         return $this->model->parent;
     }
 
-    /**
-     * @return mixed
-     */
+
     public function findChildren()
     {
         return $this->model->children;
